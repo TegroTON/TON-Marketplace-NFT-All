72395390f6052df57d7d108ff8b83cd566ab4dfa
Anton Fediushin <imantonmeep@gmail.com>
2022-05-21 12:44:38 +0200

    Move NFTItem-related things into a separate class

---
+++ nft_tool/src/jvmMain/kotlin/money/tegro/market/nft_tool/NFTItem.kt
@@ -0,0 +1,97 @@
+package money.tegro.market.nft_tool
+
+import org.ton.block.MsgAddressInt
+import org.ton.cell.BagOfCells
+import org.ton.cell.Cell
+import org.ton.cell.CellBuilder
+import org.ton.cell.CellSlice
+import org.ton.lite.api.LiteApi
+import org.ton.lite.api.liteserver.LiteServerAccountId
+
+data class NFTItem(
+    val address: MsgAddressInt.AddrStd,
+    val initialized: Boolean,
+    val index: Int,
+    val collection: MsgAddressInt.AddrStd?,
+    val owner: MsgAddressInt.AddrStd,
+    val content: Cell
+) {
+    override fun toString(): String =
+        "NFTItem(address=$address, initialized=$initialized, index=$index, collection=$collection, owner=$owner, content=$content)"
+
+    companion object {
+        @JvmStatic
+        suspend fun fetch(
+            liteClient: LiteApi,
+            address: MsgAddressInt.AddrStd
+        ): NFTItem {
+            val lastBlock = liteClient.getMasterchainInfo().last
+
+            val accountId = LiteServerAccountId(address.workchain_id, address.address)
+
+            val response = liteClient.runSmcMethod(
+                0b100, // we only care about the result
+                lastBlock,
+                accountId,
+                102351L, // get_nft_data
+                BagOfCells(
+                    CellBuilder.beginCell()
+                        .storeUInt(0, 16)
+                        .storeUInt(0, 8)
+                        .endCell() // no parameters
+                ).toByteArray()
+            )
+            require(response.exitCode == 0) { "Failed to run the method, exit code is ${response.exitCode}" }
+            var loader = BagOfCells(response.result!!).roots.first().beginParse()
+            loader.loadUInt(16) // skip whatever this is
+            loader.loadUInt(8) // number of entries
+
+            loader.loadUInt(8) // type of the last entry, going backwards here
+            var next = loader.loadRef()
+            val content = loader.loadRef()
+            loader = next.beginParse()
+
+            loader.loadUInt(8)
+            next = loader.loadRef()
+            var begin = loader.loadUInt(10).toInt()
+            var end = loader.loadUInt(10).toInt()
+            val owner = toAddress(Cell(loader.loadRef().bits.slice(begin..end)).beginParse())
+            loader = next.beginParse()
+
+            loader.loadUInt(8)
+            next = loader.loadRef()
+            begin = loader.loadUInt(10).toInt()
+            end = loader.loadUInt(10).toInt()
+            val collection = toAddress(Cell(loader.loadRef().bits.slice(begin..end)).beginParse())
+            loader = next.beginParse()
+
+            loader.loadUInt(8)
+            next = loader.loadRef()
+            val index = loader.loadUInt(64).toInt()
+            loader = next.beginParse()
+
+            loader.loadUInt(8)
+            next = loader.loadRef()
+            val initialized = loader.loadInt(64).toInt()
+
+            return NFTItem(
+                address,
+                initialized == -1,
+                index,
+                collection,
+                owner,
+                content
+            )
+        }
+
+        @JvmStatic
+        fun toAddress(slice: CellSlice): MsgAddressInt.AddrStd {
+            slice.loadBits(3) // addr_std: 10 + 0 for no anycast
+            return MsgAddressInt.AddrStd(
+                null,
+                slice.loadInt(8).toInt(),
+                slice.loadBitString(256).toByteArray()
+            )
+        }
+    }
+}
--- nft_tool/src/jvmMain/kotlin/money/tegro/market/nft_tool/main.kt
+++ nft_tool/src/jvmMain/kotlin/money/tegro/market/nft_tool/main.kt
@@ -6,14 +6,8 @@ import kotlinx.cli.Subcommand
 import kotlinx.cli.default
 import kotlinx.coroutines.runBlocking
 import org.ton.block.MsgAddressInt
-import org.ton.cell.BagOfCells
-import org.ton.cell.Cell
-import org.ton.cell.CellBuilder
-import org.ton.cell.CellSlice
 import org.ton.crypto.hex
-import org.ton.lite.api.liteserver.LiteServerAccountId
 import org.ton.lite.client.LiteClient
-import java.time.Instant
 
 suspend fun main(args: Array<String>) {
     var parser = ArgParser("nft_tool")
@@ -31,65 +25,12 @@ suspend fun main(args: Array<String>) {
         override fun execute() = runBlocking {
             val liteClient = LiteClient(liteServerHost, liteServerPort, hex(liteServerPubKey)).connect()
 
-            val time = liteClient.getTime()
-            println("[server time: $time] (${Instant.ofEpochSecond(time.now.toLong())})")
-
-            val lastBlock = liteClient.getMasterchainInfo().last
-            println("last block: $lastBlock")
-
-            val addr = MsgAddressInt.AddrStd.parse(address)
-            val accountId = LiteServerAccountId(addr.workchain_id, addr.address)
-
-            val response = liteClient.runSmcMethod(
-                0b100, // we only care about the result
-                lastBlock,
-                accountId,
-                102351L, // get_nft_data
-                BagOfCells(
-                    CellBuilder.beginCell()
-                        .storeUInt(0, 16)
-                        .storeUInt(0, 8)
-                        .endCell() // no parameters
-                ).toByteArray()
-            )
-            println(response)
-            require(response.exitCode == 0) { "Failed to run the method, exit code is ${response.exitCode}" }
-            var loader = BagOfCells(response.result!!).roots.first().beginParse()
-            loader.loadUInt(16) // skip whatever this is
-            loader.loadUInt(8) // number of entries
-
-            loader.loadUInt(8) // type of the last entry, going backwards here
-            var next = loader.loadRef()
-            val content = loader.loadRef()
-            loader = next.beginParse()
-
-            loader.loadUInt(8)
-            next = loader.loadRef()
-            var begin = loader.loadUInt(10).toInt()
-            var end = loader.loadUInt(10).toInt()
-            val owner = toAddress(Cell(loader.loadRef().bits.slice(begin..end)).beginParse())
-            loader = next.beginParse()
-
-            loader.loadUInt(8)
-            next = loader.loadRef()
-            begin = loader.loadUInt(10).toInt()
-            end = loader.loadUInt(10).toInt()
-            val collection = toAddress(Cell(loader.loadRef().bits.slice(begin..end)).beginParse())
-            loader = next.beginParse()
-
-            loader.loadUInt(8)
-            next = loader.loadRef()
-            val index = loader.loadUInt(64).toInt()
-            loader = next.beginParse()
-
-            loader.loadUInt(8)
-            next = loader.loadRef()
-            val initialized = loader.loadInt(64).toInt()
-            println("NFT Item ${addr.toString(userFriendly = true)}:")
-            println("\tInitialized: ${initialized == -1}")
-            println("\tIndex: ${index}")
-            println("\tCollection Address: ${collection.toString(userFriendly = true)}")
-            println("\tOwner Address: ${owner.toString(userFriendly = true)}")
+            val item = NFTItem.fetch(liteClient, MsgAddressInt.AddrStd.parse(address))
+            println("NFT Item ${item.address.toString(userFriendly = true)}:")
+            println("\tInitialized: ${item.initialized}")
+            println("\tIndex: ${item.index}")
+            println("\tCollection Address: ${item.collection?.toString(userFriendly = true)}")
+            println("\tOwner Address: ${item.owner.toString(userFriendly = true)}")
         }
     }
 
@@ -98,13 +39,4 @@ suspend fun main(args: Array<String>) {
     parser.subcommands(query)
 
     parser.parse(args)
-}
-
-fun toAddress(slice: CellSlice): MsgAddressInt.AddrStd {
-    slice.loadBits(3) // addr_std: 10 + 0 for no anycast
-    return MsgAddressInt.AddrStd(
-        null,
-        slice.loadInt(8).toInt(),
-        slice.loadBitString(256).toByteArray()
-    )
 }
