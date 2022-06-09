f8709e80f914aa927641bfe2446bee17d0c35ce7
Anton F <imantonmeep@gmail.com>
2022-06-09 10:14:02 +0200

    Bump ton-kotlin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,7 +31,7 @@ dependencyResolutionManagement {
             version("serialization", "1.3.2")
             version("slf4j", "1.7.36")
             version("sqlite", "3.36.0")
-            version("ton", "c4f0a99d43")
+            version("ton", "597de41105")
 
             library("clikt", "com.github.ajalt.clikt", "clikt").versionRef("clikt")
             library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core").version {
