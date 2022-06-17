5a99a1ba00ba687a80b5127424aab8f891a4da17
Anton F <imantonmeep@gmail.com>
2022-06-17 14:43:16 +0200

    Parallel steps

--- nightcrawler/src/main/kotlin/money/tegro/market/nightcrawler/job/UpdateCollectionsJob.kt
+++ nightcrawler/src/main/kotlin/money/tegro/market/nightcrawler/job/UpdateCollectionsJob.kt
@@ -3,14 +3,18 @@ package money.tegro.market.nightcrawler.job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.job.builder.FlowBuilder
+import org.springframework.batch.core.job.flow.support.SimpleFlow
 import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.task.TaskExecutor
 
 @Configuration
 @EnableBatchProcessing
 class UpdateCollectionsJob(
     private val jobBuilderFactory: JobBuilderFactory,
+    private val taskExecutor: TaskExecutor,
 
     private val updateCollectionInfo: Step,
     private val updateCollectionRoyalty: Step,
@@ -20,9 +24,27 @@ class UpdateCollectionsJob(
     @Bean
     fun updateCollections() = jobBuilderFactory.get("updateCollections")
         .incrementer(RunIdIncrementer())
-        .start(updateCollectionInfo)
-        .next(updateCollectionRoyalty)
-        .next(updateCollectionMetadata)
-        .next(discoverMissingCollectionItems)
+        .start(
+            FlowBuilder<SimpleFlow>("updateCollectionInfoFlow")
+                .start(updateCollectionInfo)
+                .build()
+        )
+        .next(
+            FlowBuilder<SimpleFlow>("parallelUpdateCollectionRoyaltyMetadataItemsFlow")
+                .split(taskExecutor)
+                .add(
+                    FlowBuilder<SimpleFlow>("updateCollectionRoyaltyFlow")
+                        .start(updateCollectionRoyalty)
+                        .build(),
+                    FlowBuilder<SimpleFlow>("updateCollectionMetadataFlow")
+                        .start(updateCollectionMetadata)
+                        .build(),
+                    FlowBuilder<SimpleFlow>("discoverMissingCollectionItemsFlow")
+                        .start(discoverMissingCollectionItems)
+                        .build(),
+                )
+                .build()
+        )
+        .build()
         .build()
 }
--- nightcrawler/src/main/kotlin/money/tegro/market/nightcrawler/job/UpdateItemsJob.kt
+++ nightcrawler/src/main/kotlin/money/tegro/market/nightcrawler/job/UpdateItemsJob.kt
@@ -3,14 +3,18 @@ package money.tegro.market.nightcrawler.job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.job.builder.FlowBuilder
+import org.springframework.batch.core.job.flow.support.SimpleFlow
 import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.task.TaskExecutor
 
 @Configuration
 @EnableBatchProcessing
 class UpdateItemsJob(
     private val jobBuilderFactory: JobBuilderFactory,
+    private val taskExecutor: TaskExecutor,
 
     private val updateItemInfo: Step,
     private val updateItemRoyalty: Step,
@@ -20,9 +24,27 @@ class UpdateItemsJob(
     @Bean
     fun updateItems() = jobBuilderFactory.get("updateItems")
         .incrementer(RunIdIncrementer())
-        .start(updateItemInfo)
-        .next(updateItemRoyalty)
-        .next(updateItemMetadata)
-        .next(updateItemSale)
+        .start(
+            FlowBuilder<SimpleFlow>("updateItemInfoFlow")
+                .start(updateItemInfo)
+                .build()
+        )
+        .next(
+            FlowBuilder<SimpleFlow>("parallelUpdateItemRoyaltyMetadataSaleFlow")
+                .split(taskExecutor)
+                .add(
+                    FlowBuilder<SimpleFlow>("updateItemRoyaltyFlow")
+                        .start(updateItemRoyalty)
+                        .build(),
+                    FlowBuilder<SimpleFlow>("updateItemMetadataFlow")
+                        .start(updateItemMetadata)
+                        .build(),
+                    FlowBuilder<SimpleFlow>("updateItemSaleFlow")
+                        .start(updateItemSale)
+                        .build(),
+                )
+                .build()
+        )
+        .build()
         .build()
 }
