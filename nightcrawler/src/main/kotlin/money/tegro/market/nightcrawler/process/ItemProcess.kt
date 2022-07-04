1f6eafc68d09260a729ebdeea6bcc995c7504aae
Anton F <imantonmeep@gmail.com>
2022-07-04 17:13:08 +0200

    Add attributes

--- nightcrawler/src/main/kotlin/money/tegro/market/nightcrawler/process/ItemProcess.kt
+++ nightcrawler/src/main/kotlin/money/tegro/market/nightcrawler/process/ItemProcess.kt
@@ -5,21 +5,32 @@ import kotlinx.coroutines.reactor.mono
 import money.tegro.market.blockchain.nft.NFTItem
 import money.tegro.market.blockchain.referenceBlock
 import money.tegro.market.core.dto.toSafeBounceable
+import money.tegro.market.core.model.AttributeModel
 import money.tegro.market.core.model.ItemModel
+import money.tegro.market.core.repository.AttributeRepository
 import mu.KLogging
 import org.ton.api.tonnode.TonNodeBlockIdExt
 import org.ton.lite.api.LiteApi
+import reactor.kotlin.core.publisher.toFlux
 
 @Prototype
 class ItemProcess(
     private val liteApi: LiteApi,
+    private val attributeRepository: AttributeRepository
 ) {
     operator fun invoke(referenceBlock: suspend () -> TonNodeBlockIdExt = liteApi.referenceBlock()) =
         { it: ItemModel ->
             mono {
                 val item = NFTItem.of(it.address.to(), liteApi, referenceBlock)
+                val metadata = item.metadata(liteApi, referenceBlock)
 
-                it.copy(item)?.copy(item.metadata(liteApi, referenceBlock))
+                metadata.attributes.orEmpty().toFlux()
+                    .map { attribute -> AttributeModel(it.address, attribute) }
+                    .subscribe {
+                        attributeRepository.upsert(it).subscribe()
+                    }
+
+                it.copy(item)?.copy(metadata)
                     ?: it.apply {
                         logger.warn { "Could not update item ${it.address.toSafeBounceable()}, something went wrong" }
                     }
