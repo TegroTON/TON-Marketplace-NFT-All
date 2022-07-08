3d6e83e5c8605dc796ebb2834820ed9acec11de3
Anton F <imantonmeep@gmail.com>
2022-07-08 08:02:10 +0200

    Encode addresses as full bocs for portability

--- core/src/main/kotlin/money/tegro/market/core/converter/AddrStdAttributeConverter.kt
+++ core/src/main/kotlin/money/tegro/market/core/converter/AddrStdAttributeConverter.kt
@@ -3,9 +3,8 @@ package money.tegro.market.core.converter
 import io.micronaut.core.convert.ConversionContext
 import io.micronaut.data.model.runtime.convert.AttributeConverter
 import jakarta.inject.Singleton
-import org.ton.bitstring.BitString
 import org.ton.block.AddrStd
-import org.ton.cell.Cell
+import org.ton.boc.BagOfCells
 import org.ton.cell.CellBuilder
 import org.ton.tlb.loadTlb
 import org.ton.tlb.storeTlb
@@ -13,10 +12,10 @@ import org.ton.tlb.storeTlb
 @Singleton
 class AddrStdAttributeConverter : AttributeConverter<AddrStd, ByteArray> {
     override fun convertToPersistedValue(entityValue: AddrStd?, context: ConversionContext): ByteArray? =
-        entityValue?.let { CellBuilder.createCell { storeTlb(addrStdCodec, it) }.bits.toByteArray() }
+        entityValue?.let { BagOfCells(CellBuilder.createCell { storeTlb(addrStdCodec, it) }).toByteArray() }
 
     override fun convertToEntityValue(persistedValue: ByteArray?, context: ConversionContext): AddrStd? =
-        persistedValue?.let { Cell(BitString(it)).beginParse().loadTlb(addrStdCodec) }  // TODO: proper parsing
+        persistedValue?.let { BagOfCells(it).roots.first().parse { loadTlb(addrStdCodec) } }
 
     companion object {
         val addrStdCodec = AddrStd.tlbCodec()
--- core/src/main/kotlin/money/tegro/market/core/converter/MsgAddressAttributeConverter.kt
+++ core/src/main/kotlin/money/tegro/market/core/converter/MsgAddressAttributeConverter.kt
@@ -3,9 +3,8 @@ package money.tegro.market.core.converter
 import io.micronaut.core.convert.ConversionContext
 import io.micronaut.data.model.runtime.convert.AttributeConverter
 import jakarta.inject.Singleton
-import org.ton.bitstring.BitString
 import org.ton.block.MsgAddress
-import org.ton.cell.Cell
+import org.ton.boc.BagOfCells
 import org.ton.cell.CellBuilder
 import org.ton.tlb.loadTlb
 import org.ton.tlb.storeTlb
@@ -13,10 +12,10 @@ import org.ton.tlb.storeTlb
 @Singleton
 class MsgAddressAttributeConverter : AttributeConverter<MsgAddress, ByteArray> {
     override fun convertToPersistedValue(entityValue: MsgAddress?, context: ConversionContext): ByteArray? =
-        entityValue?.let { CellBuilder.createCell { storeTlb(msgAddressCodec, it) }.bits.toByteArray() }
+        entityValue?.let { BagOfCells(CellBuilder.createCell { storeTlb(msgAddressCodec, it) }).toByteArray() }
 
     override fun convertToEntityValue(persistedValue: ByteArray?, context: ConversionContext): MsgAddress? =
-        persistedValue?.let { Cell(BitString(it)).beginParse().loadTlb(msgAddressCodec) }  // TODO: proper parsing
+        persistedValue?.let { BagOfCells(it).roots.first().parse { loadTlb(msgAddressCodec) } }
 
     companion object {
         val msgAddressCodec = MsgAddress.tlbCodec()
