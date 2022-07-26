f301d8bd0e2eafced4045ce3f7c9920e2aafc904
Anton F <imantonmeep@gmail.com>
2022-07-26 16:24:53 +0200

    fixes

--- src/main/kotlin/money/tegro/market/config/ServiceConfig.kt
+++ src/main/kotlin/money/tegro/market/config/ServiceConfig.kt
@@ -6,16 +6,16 @@ import java.time.Duration
 
 @ConfigurationProperties("market.service")
 interface ServiceConfig {
-    @get:Bindable(defaultValue = "PT10M")
+    @get:Bindable(defaultValue = "PT1H")
     val accountPeriod: Duration
 
-    @get:Bindable(defaultValue = "PT10M")
+    @get:Bindable(defaultValue = "PT1H")
     val collectionPeriod: Duration
 
-    @get:Bindable(defaultValue = "PT10M")
+    @get:Bindable(defaultValue = "PT1H")
     val itemPeriod: Duration
 
-    @get:Bindable(defaultValue = "PT10M")
+    @get:Bindable(defaultValue = "PT1H")
     val missingItemPeriod: Duration
 
     @get:Bindable(defaultValue = "PT10M")
