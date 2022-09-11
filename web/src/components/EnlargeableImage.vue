3d147dfc0424294e9165620c5760e085ed992b71
Anton F <imantonmeep@gmail.com>
2022-09-11 20:39:31 +0200

    Fix truncation, enlargeable image

---
+++ web/src/components/EnlargeableImage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <img :src="src" alt="" class="item-details__image" @click="this.toggle()">
+    <div v-show="isOpen" :style="{ background: 'rgba(5, 11, 28, .5) url(' + src + ') no-repeat center'}"
+         class="item-details__image--size"
+         @click="this.toggle()"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "EnlargeableImage",
+  props: {
+    src: String,
+  },
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.item-details__image--size {
+  background-size: contain;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: 10000;
+  top: 0;
+  left: 0;
+  cursor: zoom-out;
+}
+</style>
--- web/src/pages/Item.vue
+++ web/src/pages/Item.vue
@@ -8,14 +8,14 @@
               <ol class="breadcrumb mb-5 ms-3">
                 <li class="breadcrumb-item"><a href="/explore.php">Explore</a></li>
                 <li class="breadcrumb-item"><a href="/collection.php">Tegro Cat</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Tegro Cat #133</li>
+                <li aria-current="page" class="breadcrumb-item active">Tegro Cat #133</li>
               </ol>
             </nav>
             <div class="row justify-content-center mb-4">
               <div class="col-lg-5 col-xl-5 mb-4 mb-lg-0">
                 <!-- Start NFT Image -->
                 <div class="position-sticky" style="top: 140px;">
-                  <img class="item-details__image" data-enlargable src="assets/img/item-details-image.jpg" alt="">
+                  <enlargeable-image src="assets/img/item-details-image.jpg"></enlargeable-image>
                 </div>
                 <!-- // End NFT Image -->
               </div>
@@ -32,24 +32,24 @@
                 <!-- // End Desc Card -->
                 <!-- Start Action Link -->
                 <div class="card-item-details d-flex align-items-center border bg-soft px-4 py-3 rounded mb-4">
-                  <button type="button" class="btn btn-sm btn-details__like bg-soft px-3">
+                  <button class="btn btn-sm btn-details__like bg-soft px-3" type="button">
                     <i class="fa-regular fa-heart m-0 me-sm-2"></i>
                     <span class="d-block d-sm-inline mt-1 mt-sm-0">24</span>
                   </button>
                   <div class="nav-item-details d-flex align-items-center fs-18 ms-auto">
-                    <button type="button" class="btn btn-sm px-2 px-3 me-1"><i
+                    <button class="btn btn-sm px-2 px-3 me-1" type="button"><i
                         class="fa-regular fa-arrows-rotate d-block d-sm-inline mb-2 mt-sm-0 m-0 me-sm-2"></i> Refresh
                     </button>
-                    <button type="button" class="btn btn-sm px-2 px-3 me-1" data-bs-toggle="modal"
-                            data-bs-target="#ShareModal"><i
+                    <button class="btn btn-sm px-2 px-3 me-1" data-bs-target="#ShareModal" data-bs-toggle="modal"
+                            type="button"><i
                         class="fa-regular fa-share-nodes d-block d-sm-inline mb-2 mt-sm-0 m-0 me-sm-2"></i> Share
                     </button>
                     <div class="dropdown">
-                      <button class="btn btn-sm btn-cube" type="button" id="dropdownMenuButton1"
-                              data-bs-toggle="dropdown" aria-expanded="false">
+                      <button id="dropdownMenuButton1" aria-expanded="false" class="btn btn-sm btn-cube"
+                              data-bs-toggle="dropdown" type="button">
                         <i class="fa-solid fa-ellipsis fs-20"></i>
                       </button>
-                      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                      <ul aria-labelledby="dropdownMenuButton1" class="dropdown-menu">
                         <li><a class="dropdown-item" href="#">Place floor bid</a></li>
                         <li><a class="dropdown-item" href="#">New bid</a></li>
                         <li><a class="dropdown-item" href="#">Report</a></li>
@@ -68,20 +68,20 @@
                     <span class="color-grey">Plus a network fee of 1 TON</span>
                     <span class="price-item__dollar d-block color-grey text-end ms-auto">$64.09</span>
                   </div>
-                  <button type="button" class="btn btn-primary w-100" data-bs-toggle="modal"
-                          data-bs-target="#BuyNowModal">Buy Now
+                  <button class="btn btn-primary w-100" data-bs-target="#BuyNowModal" data-bs-toggle="modal"
+                          type="button">Buy Now
                   </button>
                 </div>
                 <!-- // End Price Card -->
                 <!-- Start Owner & Collection -->
                 <div class="row">
                   <div class="col-xl-6 mx-auto mb-4">
-                    <a href="/profile.php"
-                       class="card-item-details d-block border bg-soft hover px-4 py-3 rounded text-white">
+                    <a class="card-item-details d-block border bg-soft hover px-4 py-3 rounded text-white"
+                       href="/profile.php">
                       <h4 class="fs-14 color-grey mb-4">Owner</h4>
                       <div class="d-flex align-items-center">
-                        <img src="assets/img/author/author-12.jpg" alt="" class="img-fluid rounded-circle" width="40"
-                             height="40">
+                        <img alt="" class="img-fluid rounded-circle" height="40" src="assets/img/author/author-12.jpg"
+                             width="40">
                         <h4 class="collection__name fs-16 mb-0 ms-3">
                           NFT Tegro
                         </h4>
@@ -90,11 +90,11 @@
                     </a>
                   </div>
                   <div class="col-xl-6 mb-4">
-                    <a href="/collection.php"
-                       class="card-item-details d-block border bg-soft hover px-4 py-3 rounded text-white">
+                    <a class="card-item-details d-block border bg-soft hover px-4 py-3 rounded text-white"
+                       href="/collection.php">
                       <h4 class="fs-14 color-grey mb-4">Collection</h4>
                       <div class="d-flex align-items-center">
-                        <img src="assets/img/author/author-17.jpg" alt="" class="img-fluid rounded-circle" width="40">
+                        <img alt="" class="img-fluid rounded-circle" src="assets/img/author/author-17.jpg" width="40">
                         <h4 class="collection__name fs-16 mb-0 ms-3">
                           Tegro Cat
                         </h4>
@@ -109,8 +109,9 @@
                   <h4 class="fs-18 mb-4">Details</h4>
                   <ul class="list-unstyled">
                     <li class="mb-3">
-                      <a href="https://tonscan.org/address/EQC_-u5FytW3WrGR_UQ_tjVFvFbcIanSh4nHjqP3ojIamkGP"
-                         class="d-flex align-items-center p-3 rounded border hover text-white" target="_blank">
+                      <a class="d-flex align-items-center p-3 rounded border hover text-white"
+                         href="https://tonscan.org/address/EQC_-u5FytW3WrGR_UQ_tjVFvFbcIanSh4nHjqP3ojIamkGP"
+                         target="_blank">
                         <span class="fw-medium color-grey">Contract Address</span>
                         <span
                             class="col-4 text-truncate ms-auto">EQC_-u5FytW3WrGR_UQ_tjVFvFbcIanSh4nHjqP3ojIamkGP</span>
@@ -118,8 +119,9 @@
                       </a>
                     </li>
                     <li class="mb-3">
-                      <a href="https://tonscan.org/address/EQC_-u5FytW3WrGR_UQ_tjVFvFbcIanSh4nHjqP3ojIamkGP"
-                         class="d-flex align-items-center p-3 rounded border hover text-white" target="_blank">
+                      <a class="d-flex align-items-center p-3 rounded border hover text-white"
+                         href="https://tonscan.org/address/EQC_-u5FytW3WrGR_UQ_tjVFvFbcIanSh4nHjqP3ojIamkGP"
+                         target="_blank">
                         <span class="fw-medium color-grey">Sale Contract</span>
                         <span
                             class="col-4 text-truncate ms-auto">EQC_-u5FytW3WrGR_UQ_tjVFvFbcIanSh4nHjqP3ojIamkGP</span>
@@ -144,7 +146,7 @@
               <h4 class="fs-18 mb-4">Attributes</h4>
               <ul class="row list-unstyled">
                 <li class="col-lg-6 col-xl-4 mb-3">
-                  <a href="#!" class="d-flex align-items-center p-3 rounded border hover text-white" target="_blank">
+                  <a class="d-flex align-items-center p-3 rounded border hover text-white" href="#!" target="_blank">
                     <div>
                       <span class="d-block fs-14 fw-medium color-grey mb-2">Background</span>
                       <span class="d-block fw-medium">Orange</span>
@@ -161,7 +163,7 @@
                   </a>
                 </li>
                 <li class="col-lg-6 col-xl-4 mb-3">
-                  <a href="#!" class="d-flex align-items-center p-3 rounded border hover text-white" target="_blank">
+                  <a class="d-flex align-items-center p-3 rounded border hover text-white" href="#!" target="_blank">
                     <div>
                       <span class="d-block fs-14 fw-medium color-grey mb-2">Expression</span>
                       <span class="d-block fw-medium">Brown Eyes</span>
@@ -178,7 +180,7 @@
                   </a>
                 </li>
                 <li class="col-lg-6 col-xl-4 mb-3">
-                  <a href="#!" class="d-flex align-items-center p-3 rounded border hover text-white" target="_blank">
+                  <a class="d-flex align-items-center p-3 rounded border hover text-white" href="#!" target="_blank">
                     <div>
                       <span class="d-block fs-14 fw-medium color-grey mb-2">Hairstyle</span>
                       <span class="d-block fw-medium">Double Ponies</span>
@@ -195,7 +197,7 @@
                   </a>
                 </li>
                 <li class="col-lg-6 col-xl-4 mb-3">
-                  <a href="#!" class="d-flex align-items-center p-3 rounded border hover text-white" target="_blank">
+                  <a class="d-flex align-items-center p-3 rounded border hover text-white" href="#!" target="_blank">
                     <div>
                       <span class="d-block fs-14 fw-medium color-grey mb-2">Outfit</span>
                       <span class="d-block fw-medium">Qipao</span>
@@ -212,7 +214,7 @@
                   </a>
                 </li>
                 <li class="col-lg-6 col-xl-4 mb-3">
-                  <a href="#!" class="d-flex align-items-center p-3 rounded border hover text-white" target="_blank">
+                  <a class="d-flex align-items-center p-3 rounded border hover text-white" href="#!" target="_blank">
                     <div>
                       <span class="d-block fs-14 fw-medium color-grey mb-2">Prop</span>
                       <span class="d-block fw-medium">Lolipop</span>
@@ -238,11 +240,11 @@
               <table class="table text-white mb-0">
                 <thead>
                 <tr class="border-bottom">
-                  <th scope="col" class="p-4">Type</th>
-                  <th scope="col" class="p-4">Price</th>
-                  <th scope="col" class="p-4">From</th>
-                  <th scope="col" class="p-4">To</th>
-                  <th scope="col" class="p-4">Date</th>
+                  <th class="p-4" scope="col">Type</th>
+                  <th class="p-4" scope="col">Price</th>
+                  <th class="p-4" scope="col">From</th>
+                  <th class="p-4" scope="col">To</th>
+                  <th class="p-4" scope="col">Date</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -253,12 +255,12 @@
                   </td>
                   <td class="p-4">38 TON</td>
                   <td class="p-4">
-                    <a href="/profile.php" class="d-block text-truncate" target="_blank" style="max-width: 150px;">
+                    <a class="d-block text-truncate" href="/profile.php" style="max-width: 150px;" target="_blank">
                       <i class="fa-regular fa-circle-user me-2"></i>EQDZr7KDKG0R4Kyauz-iRetnuY7nMKXFIEQn4-44vzygNEvj
                     </a>
                   </td>
                   <td class="p-4">
-                    <a href="/profile.php" class="d-block text-truncate" target="_blank" style="max-width: 150px;">
+                    <a class="d-block text-truncate" href="/profile.php" style="max-width: 150px;" target="_blank">
                       <i class="fa-regular fa-circle-user me-2"></i>antonov
                     </a>
                   </td>
@@ -271,12 +273,12 @@
                   </td>
                   <td class="p-4"></td>
                   <td class="p-4">
-                    <a href="/profile.php" class="d-block text-truncate" target="_blank" style="max-width: 150px;">
+                    <a class="d-block text-truncate" href="/profile.php" style="max-width: 150px;" target="_blank">
                       <i class="fa-regular fa-circle-user me-2"></i>EQDZr7KDKG0R4Kyauz-iRetnuY7nMKXFIEQn4-44vzygNEvj
                     </a>
                   </td>
                   <td class="p-4">
-                    <a href="/profile.php" class="d-block text-truncate" target="_blank" style="max-width: 150px;">
+                    <a class="d-block text-truncate" href="/profile.php" style="max-width: 150px;" target="_blank">
                       <i class="fa-regular fa-circle-user me-2"></i>antonov
                     </a>
                   </td>
@@ -289,12 +291,12 @@
                   </td>
                   <td class="p-4">40 TON</td>
                   <td class="p-4">
-                    <a href="/profile.php" class="d-block text-truncate" target="_blank" style="max-width: 150px;">
+                    <a class="d-block text-truncate" href="/profile.php" style="max-width: 150px;" target="_blank">
                       <i class="fa-regular fa-circle-user me-2"></i>EQDZr7KDKG0R4Kyauz-iRetnuY7nMKXFIEQn4-44vzygNEvj
                     </a>
                   </td>
                   <td class="p-4">
-                    <a href="/profile.php" class="d-block text-truncate" target="_blank" style="max-width: 150px;">
+                    <a class="d-block text-truncate" href="/profile.php" style="max-width: 150px;" target="_blank">
                       <i class="fa-regular fa-circle-user me-2"></i>antonov
                     </a>
                   </td>
@@ -313,8 +315,10 @@
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import EnlargeableImage from "../components/EnlargeableImage.vue";
 
 export default defineComponent({
-  name: "Item"
+  name: "Item",
+  components: {EnlargeableImage}
 })
 </script>
