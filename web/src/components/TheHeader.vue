3d3cbaad1628a2a1d985e05f0c33bacbe98d4098
Anton F <imantonmeep@gmail.com>
2022-09-11 20:45:15 +0200

    Connect modal

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -3,20 +3,21 @@
     <div class="container-fluid">
       <nav class="navbar navbar-expand-lg d-flex align-items-center p-0">
         <a class="navbar-logo me-5" href="/">
-          <img class="navbar-logo__img" src="assets/img/logo/apple-icon-57x57.png" alt="Libermall - NFT Marketplace">
+          <img alt="Libermall - NFT Marketplace" class="navbar-logo__img" src="assets/img/logo/apple-icon-57x57.png">
           <span class="navbar-logo__name d-none d-xxl-block">Libermall</span>
         </a>
-        <button class="navbar-toggler btn btn-secondary order-2 order-xl-1 ms-3" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarHeader" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+        <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                class="navbar-toggler btn btn-secondary order-2 order-xl-1 ms-3" data-bs-target="#navbarHeader"
+                data-bs-toggle="collapse"
+                type="button">
           <i class="fa-regular fa-bars fs-20"></i>
         </button>
-        <div class="collapse navbar-collapse " id="navbarHeader">
+        <div id="navbarHeader" class="collapse navbar-collapse ">
           <form
               class="header__search flex-fill d-block d-lg-none d-xl-block mx-0 mx-lg-5 mb-3 mb-lg-0 order-1 order-lg-2">
             <div class="input-group">
-              <input type="text" class="form-control" placeholder="Search ..." aria-label="Search"
-                     aria-describedby="basic-addon2">
+              <input aria-describedby="basic-addon2" aria-label="Search" class="form-control" placeholder="Search ..."
+                     type="text">
               <div class="input-group-text p-0">
                 <button class="btn" type="submit">
                   <i class="fa-solid fa-magnifying-glass color-grey"></i>
@@ -25,12 +26,13 @@
             </div>
           </form>
           <div class="dropdown order-4 order-lg-1">
-            <button class="btn btn-primary d-flex flex-nowrap align-items-center" type="button" id="dropdownMenuCat"
-                    data-bs-toggle="dropdown" aria-expanded="false">
+            <button id="dropdownMenuCat" aria-expanded="false"
+                    class="btn btn-primary d-flex flex-nowrap align-items-center"
+                    data-bs-toggle="dropdown" type="button">
               <i class="fa-regular fa-grid-2 me-2"></i>
               Explore
             </button>
-            <ul class="dropdown-menu animate slideIn mt-3" aria-labelledby="dropdownMenuCat">
+            <ul aria-labelledby="dropdownMenuCat" class="dropdown-menu animate slideIn mt-3">
               <li><a class="dropdown-item" href="#"><i class="fa-regular fa-hexagon-vertical-nft-slanted me-3"></i>All
                 NFTs</a></li>
               <li><a class="dropdown-item" href="#"><i class="fa-regular fa-hexagon-image me-3"></i>Solanas</a></li>
@@ -61,10 +63,10 @@
           </ul>
           <!-- TODO: Show this when signed in -->
           <div v-if="false" class="dropdown dropstart order-3 order-lg-4">
-            <button type="button" id="dropdownMenuProfile" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="assets/img/user-1.svg" alt="" class="rounded-circle profile-image" width="42" height="42">
+            <button id="dropdownMenuProfile" aria-expanded="false" data-bs-toggle="dropdown" type="button">
+              <img alt="" class="rounded-circle profile-image" height="42" src="assets/img/user-1.svg" width="42">
             </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuProfile">
+            <ul aria-labelledby="dropdownMenuProfile" class="dropdown-menu">
               <li><a class="dropdown-item" href="/my-profile.php"><i class="fa-regular fa-user me-3"></i>Profile</a>
               </li>
               <li><a class="dropdown-item" href="/create-nft.php"><i
@@ -73,9 +75,9 @@
               </li>
             </ul>
           </div>
-          <button v-else type="button"
-                  class="btn btn-soft d-flex flex-nowrap align-items-center btn-mobile-fixed order-3 order-lg-4"
-                  data-bs-toggle="modal" data-bs-target="#ConnectModal">
+          <button v-else class="btn btn-soft d-flex flex-nowrap align-items-center btn-mobile-fixed order-3 order-lg-4"
+                  data-bs-target="#ConnectModal"
+                  data-bs-toggle="modal" type="button">
             <i class="fa-regular fa-arrow-right-to-arc me-2"></i>
             Connect
           </button>
@@ -83,12 +85,15 @@
       </nav>
     </div>
   </header>
+  <connect-modal></connect-modal>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import ConnectModal from "../modals/ConnectModal.vue";
 
 export default defineComponent({
-  name: 'TheHeader'
+  name: 'TheHeader',
+  components: {ConnectModal}
 })
 </script>
