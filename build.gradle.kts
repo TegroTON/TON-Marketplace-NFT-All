7faa55cc16214c78b35a68b81c16233e8785fb0f
Anton F <imantonmeep@gmail.com>
2022-09-19 12:12:54 +0200

    Fix manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,10 +26,9 @@ repositories {
 dependencies {
     implementation("io.github.microutils:kotlin-logging:2.1.23")
     implementation("net.logstash.logback:logstash-logback-encoder:7.2")
-    implementation("com.github.andreypfau.ton-kotlin:ton-kotlin:aa222bb6ca")
+    implementation("com.github.andreypfau.ton-kotlin:ton-kotlin:c678f34b0a")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
     implementation("com.expediagroup:graphql-kotlin-spring-server:6.2.1")
-    implementation(kotlin("script-runtime"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.boot:spring-boot-starter-cache")
