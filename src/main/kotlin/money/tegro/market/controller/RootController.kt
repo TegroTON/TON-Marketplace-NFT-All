7f6201342cff3be7ad08d72a92a8c55037d87e62
Anton F <imantonmeep@gmail.com>
2022-09-27 22:19:26 +0200

    Display item cards

--- src/main/kotlin/money/tegro/market/controller/RootController.kt
+++ src/main/kotlin/money/tegro/market/controller/RootController.kt
@@ -3,16 +3,19 @@ package money.tegro.market.controller
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.toList
-import money.tegro.market.service.collection.CollectionContractService
-import money.tegro.market.service.collection.CollectionItemOwnerNumberService
-import money.tegro.market.service.collection.CollectionListService
-import money.tegro.market.service.collection.CollectionMetadataService
+import money.tegro.market.service.SaleService
+import money.tegro.market.service.collection.*
+import money.tegro.market.service.item.ItemContractService
+import money.tegro.market.service.item.ItemMetadataService
+import money.tegro.market.service.item.ItemSaleAddressService
 import money.tegro.market.toRaw
 import money.tegro.market.toShortFriendly
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.ton.bigint.BigInt
+import org.ton.block.Coins
 import org.ton.block.MsgAddressInt
 
 @Controller
@@ -21,6 +24,11 @@ class RootController(
     private val collectionContractService: CollectionContractService,
     private val collectionMetadataService: CollectionMetadataService,
     private val collectionItemOwnerNumberService: CollectionItemOwnerNumberService,
+    private val collectionItemListService: CollectionItemListService,
+    private val itemContractService: ItemContractService,
+    private val itemMetadataService: ItemMetadataService,
+    private val itemSaleAddressService: ItemSaleAddressService,
+    private val saleService: SaleService,
 ) {
     @RequestMapping("/")
     suspend fun index(
@@ -64,6 +72,27 @@ class RootController(
                 "ownerShort" to (contract?.owner?.toShortFriendly() ?: "Unknown"),
                 "itemNumber" to (contract?.next_item_index ?: 0uL),
                 "itemOwnerNumber" to itemOwnerNumber,
+                "collectionItems" to collectionItemListService.get(collection)
+                    .mapNotNull { it as? MsgAddressInt }
+                    .mapNotNull { item ->
+                        val itemSale = itemSaleAddressService.get(item)?.let { saleService.get(it) }
+
+                        itemContractService.get(item)?.let { itemContract ->
+                            itemMetadataService.get(item)?.let { itemMetadata ->
+                                mapOf(
+                                    "link" to "/item/${item.toRaw()}",
+                                    "name" to itemMetadata.name,
+                                    "image" to itemMetadata.image,
+                                    "isOnSale" to (itemSale != null),
+                                    "formattedPrice" to Coins.ofNano(itemSale?.full_price ?: BigInt.ZERO)
+                                        .toString() + " TON"
+                                )
+                            }
+                        }
+
+                    }
+                    .take(10)
+                    .toList()
             )
         )
 
