c852af5340b610a992e3be0f4e9f0d2a38cf878b
Anton F <imantonmeep@gmail.com>
2022-11-04 09:18:49 +0100

    Add item index

--- src/webMain/kotlin/money/tegro/market/web/card/ItemCard.kt
+++ src/webMain/kotlin/money/tegro/market/web/card/ItemCard.kt
@@ -9,30 +9,36 @@ import money.tegro.market.web.component.Link
 import money.tegro.market.web.formatTON
 
 fun RenderContext.ItemCard(item: ItemModel) =
-    Link(setOf("item", item.address), "p-4 bg-dark-700 rounded-lg flex flex-col gap-4") {
+    Link(setOf("item", item.address), "bg-dark-700 rounded-lg relative flex flex-col") {
         picture {
-            img("w-full h-52 rounded-lg object-cover") {
+            img("w-full h-52 rounded-t-lg object-cover") {
                 src(item.image.original ?: "assets/img/user-1.svg")
             }
         }
 
-        h4("font-raleway text-lg") {
-            +item.name
+        span("absolute top-2 right-2 p-2 rounded-lg bg-soft backdrop-blur-3xl") {
+            +"#${item.index}"
         }
 
-        div("flex justify-between bg-soft rounded-xl p-4") {
-            when (item) {
-                is OrdinaryItemModel ->
-                    span("w-full text-center") {
-                        +"Not For Sale"
-                    }
+        div("p-4") {
+            h4("font-raleway text-lg") {
+                +item.name
+            }
 
-                is SaleItemModel -> {
-                    span {
-                        +"Price"
-                    }
-                    span {
-                        +item.fullPrice.formatTON().plus(" TON")
+            div("flex justify-between bg-soft rounded-xl p-4") {
+                when (item) {
+                    is OrdinaryItemModel ->
+                        span("w-full text-center") {
+                            +"Not For Sale"
+                        }
+
+                    is SaleItemModel -> {
+                        span {
+                            +"Price"
+                        }
+                        span {
+                            +item.fullPrice.formatTON().plus(" TON")
+                        }
                     }
                 }
             }
